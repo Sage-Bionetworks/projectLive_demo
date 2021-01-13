411587410aa175ad3bb8d3ac317a50848c3402c4
andrewelamb <andrewelamb@gmail.com>
2021-01-13 12:54:03 -0800

    fixed year theme and assay columns

--- data-raw/files.R
+++ data-raw/files.R
@@ -1,25 +1,21 @@
-get_created_on_year <- function(syn_id, syn){
-  e <- syn$get(syn_id)
-  e$get("createdOn") %>% 
-    stringr::str_sub(., end = 4) %>% 
-    as.integer()
-}
-
 require(magrittr)
 devtools::load_all()
 syn <- create_synapse_login()
 studies <- get_synapse_tbl(syn, "syn21918972") %>% 
   dplyr::select("grantId", "grantType")
+datasets <-  get_synapse_tbl(syn, "syn21889931") %>% 
+  dplyr::select("id", "createdOn")
 files <- get_synapse_tbl(syn, "syn21897968") %>% 
   dplyr::mutate(
     "accessType" = "PUBLIC",
-    # "year" = purrr::map_int(.data$datasetId, get_created_on_year, syn)
-    "year" = 2020,
-    "assay" = purrr::map_chr(.data$assay, purrr::pluck, 1),
-    "theme" = purrr::map_chr(.data$theme, purrr::pluck, 1),
+    # "assay" = purrr::map_chr(.data$assay, purrr::pluck, 1),
+    # "theme" = purrr::map_chr(.data$theme, purrr::pluck, 1),
     "grantId" = purrr::map_chr(.data$grantId, purrr::pluck, 1)
   ) %>% 
-  dplyr::left_join(studies)
+  dplyr::left_join(studies) %>% 
+  dplyr::left_join(datasets, by = c("datasetId" = "id")) %>% 
+  format_date_columns()
+  
 
 saveRDS(files, "files.RDS")
 store_file_in_synapse(
