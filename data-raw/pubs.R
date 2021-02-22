133ff69ad7895d4483e48e0cf5670776df059c3d
Andrew Lamb <andrewelamb@gmail.com>
2021-02-22 07:35:04 -0800

    added publicaiton stutus module

--- data-raw/pubs.R
+++ data-raw/pubs.R
@@ -1,7 +1,9 @@
 require(magrittr)
 devtools::load_all()
 syn <- create_synapse_login()
-pubs <- get_synapse_tbl(syn, "syn21868591")
+pubs <- 
+  get_synapse_tbl(syn, "syn21868591") %>% 
+  dplyr::mutate("publicationYear" = as.factor(.data$publicationYear))
 saveRDS(pubs, "pubs.RDS")
 store_file_in_synapse(
   "pubs.RDS",
